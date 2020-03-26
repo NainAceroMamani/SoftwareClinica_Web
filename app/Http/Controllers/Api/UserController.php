b105d93c87d4e0ca8855323638547b87f656d442
Nain Acero <a_nacerom@unjbg.edu.com>
2020-03-26 10:47:50 -0500

    And post user endpoint

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,8 +8,16 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function show()
-    {
+    public function show(){
+        // info del usuario que se autentifico a traves de nuestra api
         return Auth::guard('api')->user();
+    }
+
+    public function update(Request $request) {
+        $user = Auth::guard('api')->user();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+        $user->save();
     }
 }
