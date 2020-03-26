a39419d5b4be0a4f25bf9c4e3c0ad949dd5b2e1e
Nain Acero <a_nacerom@unjbg.edu.com>
2020-03-26 10:53:36 -0500

    Re-fetch user data from db

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
+use JwtAuthñ
 
 class UserController extends Controller
 {
@@ -19,5 +20,7 @@ class UserController extends Controller
         $user->phone = $request->phone;
         $user->address = $request->address;
         $user->save();
+
+        JwtAuth:clearCache($user); // el paquete guarda en cache la info de usuario por eso cuando de vuelve la info una vez actualizada es antigua por eso borramos cache para que haga la petición de nuevo 
     }
 }
